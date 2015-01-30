d33fda7c0c7693089818d31b0ca115060772ec79
William Lyu <paleneutron@outlook.com>
2015-01-30 16:44:36 +0800

    fix an error with get stuck when getting book info

--- BuilderUI.py
+++ BuilderUI.py
@@ -5,22 +5,29 @@ from PyQt5 import QtCore, QtWidgets
 
 import BuilderUI_dbg
 
+
 class BuilderUI(BuilderUI_dbg.BuilderUI_dbg):
     """Multi-thread version main UI"""
     def __init__(self):
         super(BuilderUI, self).__init__()
         self.open_url_thread = WorkThread(self.open_url)
         self.build_thread = WorkThread(self.generate_epub)
+        # 我觉得不应当用这么挫的方式，先断开再连上，应当有比较好的重载方式
+        self.pushButton_get_info.clicked.disconnect(self.open_url)
         self.pushButton_get_info.clicked.connect(self.open_url_thread.start)
 
     def build(self):
         self.pre_build()
         self.build_thread.start()
 
+
 class WorkThread(QtCore.QThread):
     def __init__(self, function):
         super(WorkThread, self).__init__()
         self.function = function
+
+    def __del__(self):
+        self.wait()
 
     def run(self):
         self.function()
